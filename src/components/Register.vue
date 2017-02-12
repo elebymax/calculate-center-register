3068be4680eb5e49da374017ee7dabd189f21deb
elebymax <m020203@gmail.com>
2017-02-12 23:00:13 +0800

    Create a component called Register

---
+++ src/components/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>hello</h2>
+</template>
+<script type="es6">
+  export default {
+    name: 'Register',
+    data () {
+      return {
+
+      }
+    },
+  }
+</script>
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
+import App from './App';
+import router from './router';
+import VueResource from 'vue-resource';
+import $ from 'jquery';
+
+Vue.use(MuseUI);
+Vue.use(VueResource);
 
 /* eslint-disable no-new */
 new Vue({
