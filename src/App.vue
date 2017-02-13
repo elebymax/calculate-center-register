27954bb3d05c33b679d3dcfaf8fbc9eef62bfa9a
elebymax <m020203@gmail.com>
2017-02-13 20:36:03 +0800

    Add new background

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <mu-appbar title="伺服器申請表單" style="background-color: #2ea1ff;">
+    <mu-appbar title="伺服器申請表單">
       <mu-icon-button icon='menu' slot="left"/>
       <mu-icon-button icon='expand_more' slot="right"/>
     </mu-appbar>
@@ -15,8 +15,12 @@ export default {
 </script>
 
 <style>
-  html, body {
-    background-color: #eee;
+  html,body {
+    background-image: url(./assets/overlay.png), linear-gradient(60deg, rgba(0, 0, 0, 0.5) 5%, rgba(255, 255, 255, 0.35)), url(./assets/bg.jpg);
+    background-repeat: repeat,	no-repeat,	no-repeat;
+    background-size: 100px 100px, cover,	cover;
+    background-position: top left,	center center,	bottom center;
+    background-attachment: fixed,	fixed,	fixed;
   }
 
   #app {
