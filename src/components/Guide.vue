7fad0e4018e73ad12f9586935e3f1e0ae38257d4
elebymax <m020203@gmail.com>
2017-02-24 18:52:44 +0800

    Add Guide.vue

---
+++ src/components/Guide.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="guide-pager">
+
+  </div>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Register from 'components/Register'
+import Guide from 'components/Guide'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Register',
       component: Register
+    },
+    {
+      path: '/guide',
+      name: 'Guide',
+      component: Guide
     }
   ]
 })
