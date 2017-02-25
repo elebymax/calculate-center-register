51db7dd50e147c363210f0dc5a5dce74bde1772b
elebymax <m020203@gmail.com>
2017-02-25 17:24:40 +0800

    Modify options in left side menu

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -2,13 +2,12 @@
   <mu-drawer width="256" :open="isMenuOpen" :docked="isDocked">
     <mu-paper>
       <mu-menu>
-        <mu-menu-item title="Preview" leftIcon="remove_red_eye"/>
-        <mu-menu-item title="Share" leftIcon="person_add"/>
-        <mu-menu-item title="Get Links" leftIcon="link"/>
+        <mu-menu-item title="帳號註冊" leftIcon="create" href="./#/register"/>
+        <mu-menu-item title="查詢帳號" leftIcon="help_outline"/>
+        <mu-menu-item title="忘記密碼" leftIcon="help"/>
+        <mu-menu-item title="使用說明" leftIcon="description" href="./#/guide"/>
         <mu-divider />
-        <mu-menu-item title="Make a copy" leftIcon="content_copy"/>
-        <mu-menu-item title="Download" leftIcon="file_download"/>
-        <mu-menu-item title="Remove" leftIcon="delete"/>
+        <mu-menu-item title="聯絡開發者" leftIcon="info"/>
       </mu-menu>
     </mu-paper>
   </mu-drawer>
