afd8439ba511e68da529289455c647202a536a4d
elebymax <m020203@gmail.com>
2017-02-25 16:36:19 +0800

    Make app bar to a component

---
+++ src/components/AppBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <mu-appbar title="伺服器申請表單" :class="[ isMenuOpen ? 'menu-show' : 'menu-hide' ]">
+    <mu-icon-button icon='menu' slot="left" @click="toggleMenu"/>
+    <mu-icon-button icon='expand_more' slot="right"/>
+  </mu-appbar>
+</template>
+<script>
+  export default {
+    name: 'AppBar',
+    props: ['isMenuOpen'],
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+      toggleMenu: function() {
+        this.$emit('toggleMenu');
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
